8055a71afab8f7e4b6bc4041ef2be3943406fbd5
Best-Turner <129537829+Best-Turner@users.noreply.github.com>
2023-08-16 20:54:38 +0300

    add ability edit user and role -'admin'

--- SpringBootProjects/SimpleSite/src/main/java/com/example/simpleSite/controllers/UserController.java
+++ SpringBootProjects/SimpleSite/src/main/java/com/example/simpleSite/controllers/UserController.java
@@ -1,17 +1,23 @@
 package com.example.simpleSite.controllers;
 
+import com.example.simpleSite.models.Role;
 import com.example.simpleSite.models.User;
 import com.example.simpleSite.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     private final UserRepo userRepo;
 
@@ -22,8 +28,31 @@ public class UserController {
 
     @GetMapping
     public String userList(Model model) {
-        List<User> users = userRepo.findAll();
-        model.addAttribute("users", users);
+        model.addAttribute("users", userRepo.findAll());
         return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+
+    @PostMapping("{user}")
+    public String userSave(@ModelAttribute User user,
+                           @RequestParam Map<String, String> role) {
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(value -> value.name())
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for(String key : role.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepo.save(user);
+        return "redirect:/user";
     }
 }
