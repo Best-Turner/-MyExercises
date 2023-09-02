3f627c5bb116d557a343a5fac4efc140971d3087
Best-Turner <129537829+Best-Turner@users.noreply.github.com>
2023-09-02 23:31:54 +0300

    We add a oneToMany relationship between the user and his messages. We make a page for user messages and a button for editing messages.

--- SpringBootProjects/SimpleSite/src/main/java/com/example/simpleSite/controller/MainController.java
+++ SpringBootProjects/SimpleSite/src/main/java/com/example/simpleSite/controller/MainController.java
@@ -4,12 +4,14 @@ import com.example.simpleSite.models.Message;
 import com.example.simpleSite.models.User;
 import com.example.simpleSite.repositories.MessageRepo;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 @Controller
@@ -63,20 +66,57 @@ public class MainController {
             model.addAttribute("messages", messageRepo.findAll());
             return "main";
         } else {
-            if (file != null && !file.isEmpty()) {
-                File uploadDir = new File(uploadPath);
-                if (!uploadDir.exists()) {
-                    uploadDir.mkdir();
-                }
-                String uuidFile = UUID.randomUUID().toString();
-                String resultFilename = uuidFile + "." + file.getOriginalFilename();
-                message.setFileName(resultFilename);
-
-                file.transferTo(new File(uploadPath + "/" + resultFilename));
-            }
+            saveFile(message, file);
             messageRepo.save(message);
         }
         return "redirect:main";
+    }
+
+    @GetMapping("/user-message/{user}")
+    public String userMessages(@AuthenticationPrincipal User currentUser,
+                               @PathVariable User user,
+                               @RequestParam(required = false) Message messageId,
+                               Model model) {
+        Set<Message> messages = user.getMessages();
+        model.addAttribute("messages", messages);
+        model.addAttribute("isCurrentUser", currentUser.equals(user));
+        model.addAttribute("message", messageId);
+        return "userMessages";
+    }
+
+    @PostMapping("/user-message/{user}")
+    public String updateMessage(@AuthenticationPrincipal User currentUser,
+                                @PathVariable Long user,
+                                @RequestParam Message messageId,
+                                @RequestParam String text,
+                                @RequestParam String tag,
+                                @RequestParam("file") MultipartFile file) throws IOException {
+        if (messageId.getAuthor().equals(currentUser)) {
+            if (!Strings.isBlank(text)) {
+                messageId.setText(text);
+            }
+            if (!Strings.isBlank(tag)) {
+                messageId.setTag(tag);
+            }
+            saveFile(messageId, file);
+            messageRepo.save(messageId);
+        }
+
+        return "redirect:/user-message/" + user;
+    }
+
+    private void saveFile(Message message, MultipartFile file) throws IOException {
+        if (file != null && !file.isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+            message.setFileName(resultFilename);
+        }
     }
 
 
