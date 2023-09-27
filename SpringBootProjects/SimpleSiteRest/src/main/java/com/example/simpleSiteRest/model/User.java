1712454fab73bcdc3b597b830a31d3959884dc98
Best-Turner <129537829+Best-Turner@users.noreply.github.com>
2023-09-27 22:10:52 +0300

    connecting Spring security and oAuth2

---
+++ SpringBootProjects/SimpleSiteRest/src/main/java/com/example/simpleSiteRest/model/User.java
@@ -0,0 +1,22 @@
+package com.example.simpleSiteRest.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private String userpic;
+    private String email;
+    private String gender;
+    private String locale;
+    private LocalDateTime lastVisit;
+}
---
+++ SpringBootProjects/SimpleSiteRest/src/main/java/com/example/simpleSiteRest/repo/UserDetailsRepo.java
@@ -0,0 +1,9 @@
+package com.example.simpleSiteRest.repo;
+
+import com.example.simpleSiteRest.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDetailsRepo extends JpaRepository<User, String> {
+}
