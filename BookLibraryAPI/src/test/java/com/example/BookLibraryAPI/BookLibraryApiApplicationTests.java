9d43f9e6b42f3152f510b0f6f5b501a1833cbbfc
Best-Turner <shurikx6881@gmail.com>
2023-10-22 14:38:47 +0300

    add gitignore

---
+++ BookLibraryAPI/src/test/java/com/example/BookLibraryAPI/BookLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookLibraryAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
